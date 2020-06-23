0a977e8253fbc6466c7b099dbd4b5075cb9a7d04
Jen <bourkj@rpi.edu>
2020-06-23 13:10:39 -0400

    Add randomized letters

--- Assets/Scripts/LetterSpawn.cs
+++ Assets/Scripts/LetterSpawn.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class LetterSpawn : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPoint = null;
-    [SerializeField] private GameObject _letter = null;
+    [SerializeField] private GameObject[] _letter = null;
     [SerializeField] private float _waitTime = 2f;
     private float _wait = 2f;
-
+    private int _index = 0;
 
     void Update()
     {
         _wait -= Time.deltaTime;
         if(_wait <= 0f){
-            Instantiate(_letter, _spawnPoint.position, Quaternion.identity);
+            _index = Random.Range(0,4);
+            Instantiate(_letter[_index], _spawnPoint.position, Quaternion.identity);
             _wait = _waitTime;
         }
     }
