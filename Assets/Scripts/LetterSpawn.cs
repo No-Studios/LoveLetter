8548f2f7eb52c6ef44d54f5807caa0b9d8ed0861
Jen <bourkj@rpi.edu>
2020-06-23 12:02:22 -0400

    Add letter spawner

---
+++ Assets/Scripts/LetterSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterSpawn : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoint = null;
+    [SerializeField] private GameObject _letter = null;
+    [SerializeField] private float _waitTime = 2f;
+    private float _wait = 2f;
+
+
+    void Update()
+    {
+        _wait -= Time.deltaTime;
+        if(_wait <= 0f){
+            Instantiate(_letter, _spawnPoint.position, Quaternion.identity);
+            _wait = _waitTime;
+        }
+    }
+}
