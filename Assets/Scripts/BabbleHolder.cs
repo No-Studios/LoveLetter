9180cd1417257f01fefd3997b83a44997c2650b9
Jen <bourkj@rpi.edu>
2020-06-24 14:04:01 -0400

    Update babble dialogue system

--- Assets/Scripts/BabbleHolder.cs
+++ Assets/Scripts/BabbleHolder.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class BabbleHolder : MonoBehaviour
 {
-    [SerializeField] private string[] _dialogue0 = null;
-    [SerializeField] private string[] _dialogue1 = null;
-    [SerializeField] private string[] _dialogue2 = null;
-    [HideInInspector] public int dialogueIndex = 0;
+    [SerializeField] private string[] _IntroDialogue = null;
+    [SerializeField] private string[] _IntroDialogue0 = null;
+    [SerializeField] private string[] _randDialogue1 = null;
+    [SerializeField] private string[] _randDialogue2 = null;
+    [SerializeField] private string[] _randDialogue3 = null;
+    [SerializeField] private string[] _randDialogue4 = null;
+    [SerializeField] private string[] _randDialogue5 = null;
 
-    // Update is called once per frame
+    [SerializeField] private string[] _windowDialogue0 = null;
+    [SerializeField] private string[] _windowDialogue1 = null;
+    [SerializeField] private string[] _windowDialogue2 = null;
+
+    [HideInInspector] public int dialogueIndex = 0;
+    private string[] _dialogueReturn;
+
+    void Update()
+    {
+
+    }
     public string[] GetDialogue()
     {
         if (dialogueIndex == 0){
-            return _dialogue0;
+            return _IntroDialogue;
         } else if (dialogueIndex == 1)
         {
-            return _dialogue1;
+            return _randDialogue1;
         } else
         {
-            return _dialogue2;
+            return _randDialogue2;
         }
 
     }
