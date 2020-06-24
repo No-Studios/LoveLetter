d310b8f08adc654d801a1c149668419e282ae2bb
Leonardo Price <leonardo.price@gmail.com>
2020-06-24 15:56:41 -0400

    writing sound effect

---
+++ Assets/writingSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class writingSound : MonoBehaviour
+{
+    [SerializeField]
+    public AudioClip[] clips ;
+    private AudioSource audioSource = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.anyKeyDown)
+        {
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+
+            }
+            else
+            {
+                AudioClip clip = GetRandomClip();
+                audioSource.pitch = (Random.Range(0.8f, 1.2f));
+                audioSource.PlayOneShot(clip);
+            }
+
+
+        }
+
+    }
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
