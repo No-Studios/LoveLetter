2df78858e8cea821793992d9218da14dd835bdeb
Jen <bourkj@rpi.edu>
2020-06-24 23:00:46 -0400

    Final penis functionality complete

--- Assets/Scripts/penis.cs
+++ Assets/Scripts/penis.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class penis : MonoBehaviour
 {
     [SerializeField] private InputField _passwordInput = null;
+    [SerializeField] private float _crotchLookTime = 0f;
+    [SerializeField] private GameObject _babbleDialogue = null;
     private bool _passwordActive = false;
     public Animation peepee;
     public GameObject cam;
@@ -19,7 +21,7 @@ public class penis : MonoBehaviour
     {
         peepee= cam.GetComponent<Animation>();
         camstart = cam.transform.position.y;
-        
+        _crotchLookTime = 4.9f;
     }
 
 
@@ -32,7 +34,7 @@ public class penis : MonoBehaviour
         } else if(Input.GetKeyDown(KeyCode.Escape) && _passwordActive == true){
             _passwordActive = false;
         }
-        if(_passwordActive == true)
+        if(_passwordActive == true && peenlever == false)
         {
             _passwordInput.gameObject.SetActive(true); 
         } else{
@@ -42,10 +44,18 @@ public class penis : MonoBehaviour
         if(_passwordInput.text == "penis" && peenlever == false && camstart <= cam.transform.position.y+.1 )
         {
             
+            _babbleDialogue.SetActive(false);
             peepee.Play("crotchShot");
-            print("fuck yeah dude");
+            _passwordInput.gameObject.SetActive(false);
             peenlever = true;
-            
+            //print("fuck yeah dude");
+        }
+        if(peepee.IsPlaying("crotchShot")){
+            _crotchLookTime -= Time.deltaTime;
+            if(_crotchLookTime <= 0f){
+                _babbleDialogue.SetActive(true);
+                _crotchLookTime = 4.9f;
+            }
         }
     }
 }
