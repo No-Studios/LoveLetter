1baee608ca7e0de90aabfc516ac8f592be4d8544
Jen <bourkj@rpi.edu>
2020-06-25 19:39:53 -0400

    Add end letter script

---
+++ Assets/Scripts/LetterFinish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterFinish : MonoBehaviour
+{
+   [SerializeField] private Animator _letterToss;
+   [SerializeField] private GameObject _kiss;
+
+    void Awake()
+    {
+        _kiss.SetActive(false);
+    }
+    
+    void EndLetter()
+    {
+        _kiss.SetActive(true);
+        _letterToss.SetBool("finished", true);
+    }
+}
