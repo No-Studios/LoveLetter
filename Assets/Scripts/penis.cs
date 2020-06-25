36eba8d818d26044cc4f3ccdcd9cbf6f6261d666
Leonardo Price <leonardo.price@gmail.com>
2020-06-24 22:17:42 -0400

    peencam implemented

--- Assets/Scripts/penis.cs
+++ Assets/Scripts/penis.cs
@@ -7,6 +7,21 @@ public class penis : MonoBehaviour
 {
     [SerializeField] private InputField _passwordInput = null;
     private bool _passwordActive = false;
+    public Animation peepee;
+    public GameObject cam;
+    private bool peenlever = false;
+    private float camstart;
+    
+    
+
+
+    void start()
+    {
+        peepee= cam.GetComponent<Animation>();
+        camstart = cam.transform.position.y;
+        
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -24,9 +39,13 @@ public class penis : MonoBehaviour
             _passwordInput.gameObject.SetActive(false);
         }
 
-        if(_passwordInput.text == "penis")
-            {
-                print("fuck yeah dude");
-            }
+        if(_passwordInput.text == "penis" && peenlever == false && camstart <= cam.transform.position.y+.1 )
+        {
+            
+            peepee.Play("crotchShot");
+            print("fuck yeah dude");
+            peenlever = true;
+            
+        }
     }
 }
